df1c69affa9c46629bbfa9828d7a174b0aa3c9c7
EDEMESSI Florian <florianedem@gmail.com>
2022-05-03 22:58:22 +0100

    Update C#_Hello_World.cs

--- C#_Hello_World.cs
+++ C#_Hello_World.cs
@@ -1,11 +1,6 @@
 using System;
-namespace HelloWorld
-{
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			Console.WriteLine("Hello, World!");
-		}
+public class Program {
+	public static void Main(string[] args) {
+		Console.WriteLine("Hello, World!");
 	}
 }
